629d66305005f061c369cc29abdd914bc845b46d
Gabriel Simmer <bladesimmer@gmail.com>
2016-05-15 10:16:24 -0700

    Added API key input (validation to do)

--- js/index.js
+++ js/index.js
@@ -10,59 +10,12 @@ var online = "Yes",
     };
 });*/
 
-fetch('http://nodemc.space:3350/log')
-.then((response) => response.text())
-.then((responseText) => {
-  $("#console").val(responseText);
-})
+// Fetch log
+setInterval(function(){ // Probably more effecient way of doing this
+    fetch('http://nodemc.space:3350/log')
+    .then((response) => response.text())
+    .then((responseText) => {
+        $("#console").val(responseText);
+    });
+}, 500);
 // ---
-// Header
-var HeaderContent = React.createClass({
-    render: function(){
-        return (
-            <div>
-            <h1>NodeMC v6 <small>Server IP: { ip } Online? { online }</small></h1>
-            </div>
-        );
-    }
-});
-// ---
-// Index page components
-var IndexContents = React.createClass({
-    render: function(){
-        return (
-            <div>
-                <ConsoleBox />
-                <CommandInput />
-            </div>
-        );
-    }
-})
-var ConsoleBox = React.createClass({
-    render: function(){
-        return (
-            <div>
-                <h4>Console</h4>
-                <textarea id="console" rows="32" className="e-log u-full-width" placeholder="Loading log..."></textarea>
-            </div>
-        );
-    }
-});
-var CommandInput = React.createClass({
-    render: function(){
-        return (
-            <div>
-                <input type="text" id="command" className="u-full-width" placeholder="command"></input>
-            </div>
-        );
-    }
-});
-// ---
-ReactDOM.render(
-    <HeaderContent />,
-    document.getElementById('header')
-);
-ReactDOM.render(
-    <IndexContents />,
-    document.getElementById('content')
-);
