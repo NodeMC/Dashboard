f146f1e9ef30c466f0edbaf77967e61d23b25121
md678685 <mathew678@hotmail.co.uk>
2017-10-04 08:24:57 +0100

    components/home: Switch to Bulma/Buefy

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,48 +1,31 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><router-link to="/login">Log in</router-link></li>
-    </ul>
-    <h2>Built with</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/ElemeFE/element" target="_blank">element</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="home">
+        <section class="hero is-primary">
+            <div class="hero-body">
+                <h1 class="title">Welcome to NodeMC</h1>
+            </div>
+        </section>
+        <footer class="footer">
+            <div class="columns">
+                <div class="column is-quarter is-offset-one-quarter">
+                    <strong>NodeMC</strong> by the <a href="https://github.com/NodeMC">NodeMC Team</a>.
+                </div>
+                <div class="column is-quarter">
+                    <h2>Built with</h2>
+                    <ul>
+                        <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+                        <li><a href="http://vuex.vuejs.org/" target="_blank">Vuex</a></li>
+                        <li><a href="https://bulma.io/" target="_blank">Bulma</a></li>
+                        <li><a href="https://buefy.github.io/" target="_blank">Buefy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </div>
 </template>
 
 <script>
 export default {
     name: "home",
-    data() {
-        return {
-            msg: "Welcome to NodeMC",
-        };
-    },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
