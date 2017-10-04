f6be903fa75785fd18054072270f562de979d9e5
md678685 <mathew678@hotmail.co.uk>
2017-10-04 08:26:36 +0100

    app: Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <navigation></navigation>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
+import Navigation from "@/components/Navigation";
+
 export default {
     name: "app",
+    components: {
+        Navigation,
+    },
 };
 </script>
 
