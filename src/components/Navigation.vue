c6c5c9abe7a2bd0d2ec912b4412b8df5e6719826
md678685 <mathew678@hotmail.co.uk>
2017-10-04 08:26:23 +0100

    components/navigation: Add navbar

---
+++ src/components/Navigation.vue
@@ -0,0 +1,50 @@
+<template>
+    <nav class="navbar" role="navigation">
+        <div class="navbar-brand">
+            <router-link class="navbar-item is-size-4" to="/">NodeMC</router-link>
+            <button class="button navbar-burger"
+                :class="{ 'is-active': hamburgerActive }"
+                @click="toggleHamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+        </div>
+        <div class="navbar-menu"
+            :class="{ 'is-active': hamburgerActive }">
+            <div class="navbar-start">
+                <!-- This space left intentionally blank -->
+            </div>
+            <div class="navbar-end">
+                <router-link class="navbar-item"
+                    to="/login"
+                    v-if="!loggedIn">
+                    <b-icon icon="person"></b-icon>
+                    <span>Log in</span>
+                </router-link>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "navigation",
+    data() {
+        return {
+            hamburgerActive: false,
+        };
+    },
+    computed: {
+        loggedIn() {
+            return this.$store.state.auth.loggedIn;
+        },
+    },
+    methods: {
+        toggleHamburger() {
+            this.hamburgerActive = !this.hamburgerActive;
+        },
+    },
+};
+</script>
+
