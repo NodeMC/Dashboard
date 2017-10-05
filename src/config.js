68346f1102051a32cd4585e526030598dc658c96
md678685 <mathew678@hotmail.co.uk>
2017-10-05 19:03:41 +0100

    config: Add config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// HTTP
+export const baseURL = "https://localhost:8081"; // Change to reflect the address and port of CORE
+
+// Authentication
+export const hawkAlgo = "sha256";
+
+// Misc
+export const anonymousCreateUser = true; // May be moved to server
