294f03482db18e9f83d70e0bdb3a86845b416342
md678685 <mathew678@hotmail.co.uk>
2017-10-10 21:58:50 +0100

    components/layout: Add CentredForm component
    Similar to login page - responsive column-based centred section

---
+++ src/components/layout/CentredForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="centred-form columns is-centered">
+        <div class="column is-half">
+            <div class="section">
+                <h1 class="title"
+                    v-if="title">{{ title }}</h1>
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["title"],
+};
+</script>
+
+<style>
+
+</style>
