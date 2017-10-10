22efd514253e23583be779e54f61d16b80fab610
md678685 <mathew678@hotmail.co.uk>
2017-10-10 21:59:13 +0100

    config: Add user creation constants

--- src/config.js
+++ src/config.js
@@ -4,5 +4,7 @@ export const baseURL = "http://localhost:8081"; // Change to reflect the address
 // Authentication
 export const hawkAlgo = "sha256";
 
-// Misc
-export const anonymousCreateUser = true; // May be moved to server
+// User creation settings (may be moved to server)
+export const anonymousCreateUser = true;
+export const minUsernameLength = 4;
+export const minPasswordLength = 8;
