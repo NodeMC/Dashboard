83a4e8254803b78d358eec6b8550a8d7d1a16484
md678685 <mathew678@hotmail.co.uk>
2017-10-11 10:03:32 +0100

    router: Use new component names pt 2

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from "vue-router";
 
 import NotFound from "@/components/NotFoundPage";
 
-import Home from "@/components/Home";
+import Home from "@/components/HomePage";
 import Login from "@/components/AuthLoginPage";
 import CreateUser from "@/components/UserCreatePage";
 import Debug from "@/components/DebugPage";
