fefd2d85399683bd3f5695a1e384c2f549b061ba
md678685 <mathew678@hotmail.co.uk>
2017-10-11 10:00:04 +0100

    store: Add username mutation

--- src/store/mutationTypes.js
+++ src/store/mutationTypes.js
@@ -2,3 +2,5 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_ERROR = "LOGIN_ERROR";
 export const LOGIN_START = "LOGIN_START";
 export const LOGIN_FINISH = "LOGIN_FINISH";
+
+export const USER_SET_USERNAME = "USER_SET_USERNAME";
