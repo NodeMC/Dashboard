aa229c4623dd8b213926f170bccf8631442da8b6
md678685 <mathew678@hotmail.co.uk>
2017-10-11 09:59:32 +0100

    components/notfound: Add "not found" page

---
+++ src/components/NotFoundPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="section has-text-centered">
+        <p class="is-size-2">There's nothing here.</p>
+        <router-link to="/" class="is-size-4">Return to homepage</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
