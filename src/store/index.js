25bf2c35b91322eda7cd68c06ba3029e989218af
md678685 <mathew678@hotmail.co.uk>
2017-10-11 10:00:52 +0100

    store: Add user module

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import auth from "./modules/auth";
+import user from "./modules/user";
 
 Vue.use(Vuex);
 
@@ -10,6 +11,7 @@ const DEBUG = process.env.NODE_ENV === "development";
 export default new Vuex.Store({
     modules: {
         auth,
+        user,
     },
     strict: DEBUG,
 });
